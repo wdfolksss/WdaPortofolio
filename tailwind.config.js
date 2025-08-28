7b795b07864184bc25313e5ab47246133c9d766f
Warda Amalia <amaliawarda411@gmail.com>
2025-08-28 17:20:20 +0700

    portofolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,26 @@ export default {
   content: [
     "./*.{html,js}",
     "./src/**/*.{html,js}"],
+    darkMode: 'class',
+  // content: ["./index.html"],
   theme: {
     extend: {
       screens: {
         '1360': '1360px',
+      },
+       colors: {
+        // part1
+        boyred: '#6A6352', 
+        vanilla: '#D5CCAB', 
+        grullo: '#A3A289',
+        green: '#878568',
+        olive: '#505143',
+        // part2
+        chinese: '#OE141C',
+        policeblue: '#314B6E',
+        rackley: '#607EA2',
+        weldonblue: '#8197AC',
+        silverpink: '#BDB3A3',
       },
       fontFamily: {
         'instrument': ['"Instrument Sans"'],
