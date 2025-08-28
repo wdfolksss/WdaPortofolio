3b733e3c59a5fd6fc84bf7d9ea906c23ef9baaf9
Warda Amalia <amaliawarda411@gmail.com>
2025-08-28 19:14:50 +0700

    fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/WdaPortofolio/', // <- sesuai nama repo GitHub kamu
+  base: '/WdaPortofolio/', // ganti sesuai nama repo GitHub kamu
 })
